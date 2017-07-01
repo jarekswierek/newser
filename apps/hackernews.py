54d58277fc661366831c2711bfdb7cb4e12f6d8c
jarecky <j.swierek@itm.biz.pl>
2017-07-01 17:53:57 +0200

    hackernews iterate

--- apps/hackernews.py
+++ apps/hackernews.py
@@ -17,10 +17,10 @@ def get_date(timestamp):
 class Item:
     """Hacker News Item.
     """
-    def __init__(self, id, title, url, type, time, *args, **kwargs):
+    def __init__(self, id, title, type, time, *args, **kwargs):
         self.id = id
         self.title = title
-        self.url = url
+        self.url = kwargs.get('url', '')
         self.type = type
         self.time = get_date(time)
 
@@ -35,12 +35,23 @@ class HackerNews(ABC):
     def __init__(self, *args, **kwargs):
         self.timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         self.items = requests.get(self.url).json()
+        self.items.reverse()
 
     def __len__(self):
         return len(self.items)
 
     def __repr__(self):
         return "{} from {!r}".format(self.name, self.timestamp)
+
+    def __getitem__(self, position):
+        return self.get_item(self.items[position])
+
+    def __reversed__(self):
+        return self[::-1]
+
+    def pop(self):
+        item_id = self.items.pop()
+        return self.get_item(item_id)
 
     def get_item(self, item_id):
         """Method return Item object with given item id.
@@ -49,6 +60,21 @@ class HackerNews(ABC):
         url = API_URL + item_url
         r = requests.get(url)
         return Item(**r.json())
+
+    @property
+    def top_ten(self):
+        """Top ten items.
+        """
+        return [self.get_item(self.items[i]) for i in range(10)]
+
+    def print_top_ten(self):
+        """Print top ten items in console.
+        """
+        for i, item in enumerate(self.top_ten, 1):
+            print("\n{} [{}]".format(i, item.time))
+            print(item)
+            if item.url:
+                print(item.url)
 
     @property
     @abstractmethod
--- main.py
+++ main.py
@@ -3,12 +3,6 @@ from apps import hackernews
 
 
 if __name__ == "__main__":
-    top = hackernews.HackerNewsTop()
-    print(top)
-    print(len(top))
     new = hackernews.HackerNewsNew()
     print(new)
-    print(len(new))
-    best = hackernews.HackerNewsBest()
-    print(best)
-    print(len(best))
+    new.print_top_ten()
